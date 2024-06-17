9bb68eb52a44eee199369b31c89231286a3f4497
AlexLing <al4423@ic.ac.uk>
2024-06-17 19:15:09 +0100

    Added comments

--- src/blink_led.s
+++ src/blink_led.s
@@ -1,1 +1,7 @@
 #COMMENTS HAVE TO BE REMOVED BEFORE COMPILING
+
+# Set bits 5-3 of word at 0x3f20 0008 to 001, this makes GIPO 21 an output
+
+# Set bit 21 of GPSET0 at 0x3f20 001c to 1 to turn light on
+
+# Set bit 21 of GPCLR0 at 0x3f20 0028 to 1 to clear signal / light off
