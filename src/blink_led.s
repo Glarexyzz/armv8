469ba541a9a6704e835e24435ee2394ff863810a
ba823 <ba823@ic.ac.uk>
2024-06-18 17:33:02 +0100

    Done blink_led.s file, not tested

--- src/blink_led.s
+++ src/blink_led.s
@@ -1,7 +1,44 @@
-#COMMENTS HAVE TO BE REMOVED BEFORE COMPILING
+initilialize:
+    ldr w1, SET_BIT3
+    ldr w2, GPFSEL2
+    str w1, [w2]
 
-# Set bits 5-3 of word at 0x3f20 0008 to 001, this makes GIPO 21 an output
+    ldr w1, SET_BIT21
+    ldr w2, GPSET0
+    ldr w3, GPCLR0
+    movz w0, #0
 
-# Set bit 21 of GPSET0 at 0x3f20 001c to 1 to turn light on
+blink_loop:
+high_signal_output:
+    str w0, [w3]
+    str w1, [w2]
+    ldr w4, WAIT_TIME
 
-# Set bit 21 of GPCLR0 at 0x3f20 0028 to 1 to clear signal / light off
+wait_high:
+    subs w4, w4, #1
+    b.ne wait_high
+
+low_signal_output:
+    str w0, [w2]
+    str w1, [w3]
+    ldr w4, WAIT_TIME
+
+wait_low:
+    subs w4, w4, #1
+    b.ne wait_low
+
+b blink_loop
+and x0, x0, x0
+
+GPFSEL2:
+    .int 0x3f200008
+GPSET0:
+    .int 0x3f20001c
+GPCLR0:
+    .int 0x3f200028
+SET_BIT3:
+    .int 0x08
+SET_BIT21:
+    .int 0X200000
+WAIT_TIME:
+    .int 0x000fffff
